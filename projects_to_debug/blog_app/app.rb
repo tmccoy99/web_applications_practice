80d6c8f3cb3778f55e6ca0d5c94ba7200c4c9777
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-11 16:01:23 +0000

    Fixed three bugs

--- projects_to_debug/blog_app/app.rb
+++ projects_to_debug/blog_app/app.rb
@@ -15,13 +15,13 @@ class Application < Sinatra::Base
   end
 
   get '/' do
-    posts = @post_manager.all_posts
+    @posts = @post_manager.all_posts
 
     return erb(:index)
   end
 
   # get posts for a given tag
-  post '/tag/:tag' do
+  get '/tag/:tag' do
     @posts = @post_manager.all_posts_by_tag(params[:tag])
 
     return erb(:index)
